ead0e93b491ec15675dfc2ed58c1569287b25fe3
Julian-Almario <julian-andres-almario@hotmail.com>
2024-08-18 17:53:14 -0500

    New icon and time moment

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from machine import Pin, I2C
 from ssd1306 import SSD1306_I2C
 import ds1302
 import time
-import dht
 import framebuf
 
 #Inicio de Display SSD1306
@@ -12,12 +11,9 @@ oled = SSD1306_I2C(128,64,i2c)
 #Inicio de RTC DS1302
 ds = ds1302.DS1302(Pin(5),Pin(18),Pin(19))  # (clk, dio, cs)
 
-#Inicio DHT11
-
 #Inicio Fotoresistor
 ldr = machine.ADC(27)
 
-#Read img
 def Abrir_Icono(ruta_icono):
     doc = open(ruta_icono, "rb")
     doc.readline()
@@ -28,27 +24,33 @@ def Abrir_Icono(ruta_icono):
     doc.close()
     return framebuf.FrameBuffer(icono, x, y, framebuf.MONO_HLSB)
 
-
-#Display Date
+def Buenas(time): #Definir estado del dia
+    if time >= 1 and time <= 12:
+        oled.text("BUENAS DIAS", 0, 0)
+    elif time >= 12 and time <= 18:
+        oled.text("BUENAS TARDES", 0, 0)
+    else:
+        oled.text("BUENAS NOCHES", 0, 0)
+        
+# Display Data
 while True:
-    #d = dht.DHT11(Pin(8))
-    #time.sleep(5)
-    #d.measure()
-    #print(d.temperature()) # eg. 23 (°C)
-    #print(d.humidity())
-    oled.text("BUENAS NOCHES :)", 0, 0)
+    Buenas(ds.hour()) #Buenas
     
-    oled.text("{}/{}/{}" .format(ds.day(),ds.month(),ds.year()), 17, 17)
-    oled.blit(Abrir_Icono("img/reloj.pbm"), 0, 25)
-    oled.text("{}:{}:{}" .format(ds.hour(), ds.minute(),ds.second()), 17,30)
+    oled.blit(Abrir_Icono("img/calendario.pbm"), 0, 16)
+    oled.text("{}/{}/{}" .format(ds.day(),ds.month(),ds.year()), 17, 20)
     
-    oled.text("{}" .format(ldr.read_u16()), 17,45)
+    oled.blit(Abrir_Icono("img/reloj.pbm"), 0, 34)
+    oled.text("{}:{}:{}" .format(ds.hour(), ds.minute(),ds.second()), 17,39)
+    
+    oled.text("{}" .format(ldr.read_u16()), 17,55)
+    
     time.sleep(1)
-    oled.show() #Renderizo texto en pantalla
-    oled.fill(0) #Limpio la pantalla para que no se superponga el texto
+    
+    oled.show() # Renderizo texto en pantalla
+    oled.fill(0) # Limpio la pantalla para que no se superponga el texto
 
 
-# Set the date and time on the RTC
+# 
 #ds.year(2024)  # Set the year to 2085
 #ds.month(8)    # Set the month to January
 #ds.day(14)     # Set the day to 17th
